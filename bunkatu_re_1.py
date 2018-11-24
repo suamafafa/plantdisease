1106fbe3ed533b962e9df3698aa7e6f468373cc8
suamafafa <suamandfafa@outlook.jp>
2018-11-24 22:24:06 +0900

    lastrun

--- bunkatu_re_1.py
+++ bunkatu_re_1.py
@@ -1,3 +1,6 @@
+#dataを使う
+#各、ランダムクロップ
+
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -47,16 +50,16 @@ start_time = time.time()
 print("start time : " + str(start_time))
 
 #params 
-csv_name = 'tomato_df_train_random_2_resize.csv'
+csv_name = 'train_tomato_col_seg_random.csv'
 #csv_name = "tomoto_and_unclass.csv"
 csv = pd.read_csv(csv_name, header=None)
 #test_csv_name = 'tomato_test_only_tomato.csv'
-test_csv_name = 'tomato_test_only_tomato_original.csv'
+test_csv_name = 'tomato_test_only_tomato_original_relabeling.csv'
 test_csv = pd.read_csv(test_csv_name, header=None)
 #path col=0 
 #label col=1
 sample_size = csv.shape[0]
-n_class = len(np.unique(csv[1]))
+n_class = len(np.unique(csv[3]))
 iteration_num = int(sample_size/a.batch_size*a.epoch)
 seed = 1141919
 
@@ -65,38 +68,62 @@ with tf.name_scope('LoadImage'):
 	filename_queue = tf.train.string_input_producer([csv_name], shuffle=True)
 	reader = tf.TextLineReader()
 	_, val = reader.read(filename_queue)
-	record_defaults = [["a"], [0], ["a"]]
-	_, label, path = tf.decode_csv(val, record_defaults=record_defaults)
+	record_defaults = [["a"],["a"],["a"],[0],["a"],["a"],["a"]]
+	_, _, _, label, path, _, _ = tf.decode_csv(val, record_defaults=record_defaults)
 	readfile = tf.read_file(path)
 	image = tf.image.decode_jpeg(readfile, channels=3)
 	image = tf.image.convert_image_dtype(image, dtype=tf.float32)
 	image = tf.cast(image, dtype=np.float32)
+	
+	
+	filename_queue_seg = tf.train.string_input_producer([csv_name], shuffle=True)
+	reader_seg = tf.TextLineReader()
+	_, val_seg = reader.read(filename_queue_seg)
+	record_defaults_seg = [["a"],["a"],["a"],[0],["a"],["a"],["a"]]
+	_, _, _, _, _ , _, seg_path = tf.decode_csv(val, record_defaults=record_defaults)
+	readfile_seg = tf.read_file(seg_path)
+	image_seg = tf.image.decode_jpeg(readfile_seg, channels=3)
+	image_seg = tf.image.convert_image_dtype(image_seg, dtype=tf.float32)
+	image_seg = tf.cast(image_seg, dtype=np.float32)
+
 	image = tf.image.resize_images(image, (256, 256))
-	def noisy(img):
-		img_f = tf.reshape(img, [-1])
-		a_m = tf.where(img_f>1/255.0, tf.zeros_like(img_f), tf.ones(img_f.shape))
-		a_m = tf.reshape(a_m, img.shape)
-		noise = tf.random_uniform(a_m.shape, minval=0, maxval=1, dtype=tf.float32)
-		mu = tf.multiply(a_m, noise)+img
+	image_seg= tf.image.resize_images(image_seg, (256, 256))
+	
+
+	def noisy(img, img_seg):	
+		noise = tf.random_uniform(img.shape, minval=0, maxval=1, dtype=tf.float32)
+		mu = tf.multiply(img_seg, noise)+img
 		return mu
+	
 	with tf.name_scope('noise_images'):
-		image = noisy(image)
+		image = noisy(image, image_seg)
+	
+	print("****", image)
+	#image = tf.image.resize_images(image, (256, 256))
 	image = tf.image.random_brightness(image, max_delta=0.25)
 	image = tf.image.random_contrast(image, lower=0.75, upper=1.25)
 	image = tf.image.adjust_hue(image, tf.random_uniform([1], minval=-0.1,maxval=0.1,dtype=tf.float32)[0])
-	#image = part(image, 180, tf.random_uniform(shape=[], minval=0,maxval=1,dtype=tf.int64), tf.random_uniform(shape=[], minval=0,maxval=1,dtype=tf.int64))
-	
-	"""
-	def contrast(img, param1, param2):
-		with tf.name_scope("contrast"):
+	h, w, ch = 256, 256, _
+	print(image.get_shape())
+	# transform params
+	CROP_SIZE = int(h)
+	SCALE_SIZE = int(h+20)
+	rot90_times = tf.random_uniform([1], 0,5,dtype=tf.int32)[0]
+	crop_offset = tf.cast(tf.floor(tf.random_uniform([2], 0, SCALE_SIZE - CROP_SIZE + 1, seed=seed)), dtype=tf.int32)
+	def transform(img, rot90_times, crop_offset,scale_size=SCALE_SIZE,crop_size=CROP_SIZE):
+		with tf.name_scope('transform'):
 			r = img
-			r = tf.image.random_brightness(r,param1) #明るさ調整
-			r = tf.image.random_contrast(r,lower=param2, upper=1/param2) #コントラスト調整
+			# rotation
+			r = tf.image.rot90(r, k=rot90_times)
+			# random crop
+			r = tf.image.resize_images(r, [scale_size, scale_size], method=tf.image.ResizeMethod.AREA)
+			r = tf.image.crop_to_bounding_box(r, crop_offset[0], crop_offset[1], crop_size, crop_size)
 			return r
-	with tf.name_scope("contrast_images"):
-		image = contrast(image, param1=0.1, param2=0.9)
-	"""
-	#label = tf.one_hot(label, depth=n_class)
+	with tf.name_scope('transform_images'):
+		image = transform(image, rot90_times, crop_offset, scale_size=SCALE_SIZE, crop_size=CROP_SIZE)	
+	image = tf.image.resize_images(image, (model_size, model_size),method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+	print("***", image)
+	label = tf.one_hot(label, depth=n_class)
 	label_batch, x_batch = tf.train.batch([label, image],batch_size=a.batch_size, allow_smaller_final_batch=False)
 	label_batch = tf.cast(label_batch, dtype=np.float32)
 
@@ -105,7 +132,7 @@ with tf.name_scope('LoadImage'):
 	test_reader = tf.TextLineReader()
 	_, test_val = test_reader.read(test_filename_queue)
 	record_defaults = [["a"], ["a"], ["a"], [0], ["a"], [0], ["a"], ["a"]]
-	_, _, _, _, _, test_label, _, test_path = tf.decode_csv(test_val, record_defaults=record_defaults)
+	_, _, _, test_label, _, _, _, test_path = tf.decode_csv(test_val, record_defaults=record_defaults)
 	test_readfile = tf.read_file(test_path)
 	test_image = tf.image.decode_jpeg(test_readfile, channels=3)
 	test_image = tf.image.convert_image_dtype(test_image, dtype=tf.float32)
@@ -116,59 +143,9 @@ with tf.name_scope('LoadImage'):
 	test_label_batch = tf.cast(test_label_batch, dtype=np.float32)
 
 am_testing = tf.placeholder(dtype=bool,shape=())
+data = tf.cond(am_testing, lambda:test_x_batch, lambda:x_batch)
 label = tf.cond(am_testing, lambda:test_label_batch, lambda:label_batch)
 drop = tf.placeholder(tf.float32) 
-
-"""
-def bunkatu(one_patch, size):
-	one = one_patch
-	for i in range(one.shape[0]):
-		for j in range(one.shape[0]):
-			tmp = one[i,j,]
-			part = tf.reshape(tmp, [size, size, 3])
-			part = tf.expand_dims(part, 0) #become 4d
-			if i==0andj==0:
-				parts = part
-			else:
-				parts = tf.concat([parts,part], 0)
-	return parts
-"""
-#add
-patch_size_t = 256*4//5
-stride = 20
-patch_t = tf.extract_image_patches(x_batch, ksizes=[1, patch_size_t, patch_size_t, 1], strides=[1,stride, stride, 1], rates=[1, 1, 1, 1], padding='VALID')
-
-print("**", patch_t)
-
-def parts(patch, size):
-	for i in range(patch.shape[0]):
-		one_patch = patch[i,:,:,:]
-		for j in range(one_patch.shape[0]):
-			for k in range(one_patch.shape[0]):
-				tmp = one_patch[j,k,]
-				part = tf.reshape(tmp, [size, size, 3])
-				part = tf.expand_dims(part, 0) #become 4d
-				if i==0 and j==0 and k==0:
-					parts = part
-				else:
-					parts = tf.concat([parts,part], 0)
-	return parts
-
-parts_t = parts(patch_t, patch_size_t)
-
-parts_t = tf.image.resize_images(parts_t, (model_size, model_size))
-
-
-def zouhuku(item):
-	return tf.fill([patch_t.shape[1]*patch_t.shape[1]], item)
-
-#label_original = label
-#label_original = tf.one_hot(label_original, depth=n_class)
-label = tf.map_fn(zouhuku, label_batch)
-label = tf.reshape(label,[-1])
-label = tf.cast(label, dtype=np.int64)
-label = tf.one_hot(label, depth=n_class)
-label = tf.cast(label, dtype=np.float32)
 
 #--------------Model-----------------#
 #QQQ
@@ -182,8 +159,7 @@ with tf.variable_scope('def_model', reuse=tf.AUTO_REUSE):
 
 with tf.name_scope('model'):
 	with tf.variable_scope('model', reuse=tf.AUTO_REUSE):
-		y = model(parts_t)
-		y_v = model(test_x_batch)
+		y = model(data)
 
 #--------------Loss&Opt-----------------#
 with tf.name_scope("cost"):
@@ -231,46 +207,6 @@ def Accuracy2(y, label):
 	accuracy = acc/len(parts_split)
 	return accuracy
 
-def Accuracy3(y_v, label): #一個でも正解があればOK
-	label = tf.argmax(label, 1) #これでshowlabelと同じ
-	eq = tf.equal(tf.argmax(y_v, 1), tf.reduce_max(label))
-	an = tf.reduce_any(eq)
-	accuracy = tf.cast(an, tf.float32)
-	return accuracy
-
-def Pred(y):
-	parts_split = tf.split(y, num_or_size_splits=1) #1枚の画像にする
-	for j in range(len(parts_split)):
-		one_part = parts_split[j]
-		regions = tf.split(one_part, num_or_size_splits=one_part.shape[0]) #同一画像内の領域群
-		for i, region in enumerate(regions):
-			if i==0:
-				max = tf.reduce_max(region)
-				pred = tf.argmax(region, 1)
-			t = tf.greater(tf.reduce_max(region), max)
-			if t is not None:
-				pred = tf.argmax(region, 1)
-				pred = tf.cast(pred, tf.float32)
-	return pred
-
-def Test_y(y):
-	parts_split = tf.split(y, num_or_size_splits=a.batch_size)
-	num = np.array([])
-	for i, one_image_y in enumerate(parts_split):
-		y_ = tf.reshape(one_image_y, [-1])
-		pred = y_[tf.argmax(y_)]
-		num = np.append(num, pred)
-		num_ = tf.convert_to_tensor(num, np.float32)
-	return num_
-def Test_label(label):
-	label_split = tf.split(label, num_or_size_splits=a.batch_size)
-	num = np.array([])
-	for i in range(a.batch_size):
-		answer = label_split[i][0]
-		num = np.append(num, answer)
-		num_ = tf.convert_to_tensor(num, np.float32)
-		return num_
-
 with tf.name_scope("accuracy"):
 	accuracy = Accuracy(y, label)
 
@@ -283,21 +219,15 @@ def Showy_raw(y):
 def Showlabel(label):
 	return tf.argmax(label,1)
 
-def Testshow(y):
-	y_ = tf.reshape(y, [-1])
-	return y, y_
-	#return tf.argmax(y_, 1)
-
-showy = Showy(y_v)
-showlabel = Showlabel(test_label_batch)
-test_acc = Accuracy(y_v, test_label_batch)
+showy = Showy(y)
+showlabel = Showlabel(label)
 
 #--------------Summary-----------------#
 with tf.name_scope('summary'):
 	with tf.name_scope('image_summary'):
-		tf.summary.image('parts_t', tf.image.convert_image_dtype(parts_t, dtype=tf.uint8, saturate=True), collections=['train'])
 		tf.summary.image('x_batch', tf.image.convert_image_dtype(x_batch, dtype=tf.uint8, saturate=True), collections=['train'])
 		tf.summary.image('test_x_batch', tf.image.convert_image_dtype(test_x_batch, dtype=tf.uint8, saturate=True), collections=['train'])
+	
 	with tf.name_scope("train_summary"):
 		cost_summary_train = tf.summary.scalar('train_loss', cost, collections=['train'])
 		acc_summary_train = tf.summary.scalar("train_accuracy", accuracy, collections=['train'])
@@ -343,40 +273,20 @@ with tf.Session(config=tmp_config) as sess:
 			sess.run(train_op, feed_dict={am_testing: False, drop:a.dropout})		
 			if step % a.print_loss_freq == 0:
 				print(step)
-				train_acc = sess.run(accuracy, feed_dict={am_testing: False, drop:0.0})
+				showy_, showlabel_, train_acc = sess.run([showy, showlabel,accuracy], feed_dict={am_testing: False, drop:0.0})
 				print("train accuracy", train_acc)
+				print(showy_)
+				print(showlabel_)
 				summary_writer.add_summary(sess.run(merged, feed_dict={am_testing:False, drop:0.0}), step)
 				
 				step_num = -(-test_csv.shape[0]//a.batch_size)
-				#step_num = 51
 				for i in range(step_num):
-					showy_, showlabel_, test_acc_ = sess.run([showy, showlabel, test_acc], feed_dict={am_testing:True, drop:0.0})
+					showy_, showlabel_, test_acc = sess.run([showy, showlabel, accuracy], feed_dict={am_testing:True, drop:0.0})
 					print(showy_)
 					print(showlabel_)
-					#softmax_each = np.argmax(softmax, 1) #各行の予測ラベル
-					#max_softmax_value = np.array([])
-					#for i in range(softmax.shape[0]):
-					#	max_softmax_value=np.append(max_softmax_value, softmax[i, softmax_each[i]]) #各マックスのソフトマックスの値
-					#max_value_idx = np.argmax(max_softmax_value)
-					#max_value_idx = np.argpartition(max_softmax_value, -1)[-1:]
-					#max_value_idx = np.argsort(max_softmax_value)[::-1]
-					#preds = softmax_each[max_value_idx]
-					#print(preds)
-					#if np.any(preds==showlabel_[0]):
-					#	check_top+=1
-
-					#if np.any(showy_==showlabel_[0]):
-					#	check+=1
-					#	print("check")
-					#print("pred", sess.run(pred, feed_dict={am_testing:True, drop:0.0}))
-					#print(sess.run(testshow, feed_dict={am_testing:True, drop:0.0}))
-					#print(sess.run(testshow2, feed_dict={am_testing:True, drop:0.0}))
 					print("")
-					#print(sess.run(test_y, feed_dict={am_testing:True, drop:0.0}))
-					#print(sess.run(test_label, feed_dict={am_testing:True, drop:0.0}))
-				print("check", test_acc_)
-				#print("check_top", check_top/step_num)
-				summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag="test_summary/test_accuracy", simple_value=test_acc_)]), step)
+				print("check", test_acc)
+				summary_writer.add_summary(tf.Summary(value=[tf.Summary.Value(tag="test_summary/test_accuracy", simple_value=test_acc)]), step)
 			
 			if step % 500 == 0:
       			# SAVE
